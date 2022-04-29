79a81832b206b7eb4ddf9b873fd0efaecba5898b
smehra18 <49688025+smehra18@users.noreply.github.com>
2022-04-29 13:27:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-from flask import Flask, render_template, request
+import flask
+from flask_cors import CORS
+import json
 
-app = Flask(__name__)
+# Initializing flask app
+app = flask.Flask(__name__)
+# Adding cors to flask
+CORS(app)
 
-@app.route("/")
-def index():    
-    return render_template("index.html")
 
+# Controller-1
+@app.route("/demo", methods=['GET'])
+def get_demo():
+    return "This is a demo api sachin"
+
+
+# Controller-2
+@app.route("/name", methods=['POST'])
+def get_demo_name():
+    data = flask.request.data
+    body = json.loads(data)
+    name = body["name"]
+    if name == "Sachin":
+        return "1"
+
+    #return "Hello, I am {}!".format(name)
+
+
+# Running the api
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
